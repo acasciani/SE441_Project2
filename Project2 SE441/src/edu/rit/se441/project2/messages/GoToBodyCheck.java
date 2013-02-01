06e0ec3efe390826ce29fdb4480c5700be2576fd
alex casciani <acc1728@rit.edu>
2013-01-31 19:59:22 -0500

    Messages sent between BodyCheck and Line fixed

--- Project2 SE441/src/edu/rit/se441/project2/messages/GoToBodyCheck.java
+++ Project2 SE441/src/edu/rit/se441/project2/messages/GoToBodyCheck.java
@@ -1,5 +1,16 @@
 package edu.rit.se441.project2.messages;
 
+import edu.rit.se441.project2.nonactors.Passenger;
+
 public class GoToBodyCheck {
+	private final Passenger passenger;
+	
+	public GoToBodyCheck(final Passenger passenger) {
+		this.passenger = passenger;
+	}
+	
+	public Passenger getPassenger() {
+		return passenger;
+	}
 
 }
