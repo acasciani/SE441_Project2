fcc3d9aaa7e23e6e10c8488c8dea644952f71c94
alex casciani <acc1728@rit.edu>
2013-01-31 21:19:52 -0500

    Need to finish and definitely refactor!!!

--- Project2 SE441/src/edu/rit/se441/project2/actors/SecurityActor.java
+++ Project2 SE441/src/edu/rit/se441/project2/actors/SecurityActor.java
@@ -1,17 +1,154 @@
 package edu.rit.se441.project2.actors;
 
+import java.util.HashMap;
+
+import akka.actor.ActorRef;
 import akka.actor.UntypedActor;
+import edu.rit.se441.project2.messages.BagCheckReport;
+import edu.rit.se441.project2.messages.BodyCheckReport;
+import edu.rit.se441.project2.messages.Register;
+import edu.rit.se441.project2.nonactors.Baggage;
+import edu.rit.se441.project2.nonactors.Passenger;
 
 public class SecurityActor extends UntypedActor {
+	private static final String PASSENGER = "Passenger";
+	private static final String BAGGAGE   = "Baggage";
 	private final int lineNumber;
+	private final HashMap<Passenger, HashMap<String, Boolean>> mapping;
+	private ActorRef jailActor;
 	
 	public SecurityActor(final int lineNumber) {
 		this.lineNumber = lineNumber;
+		// allows us to keep track of passengers to baggage
+		// this implementation only supports one bag!
+		mapping = new HashMap<Passenger, HashMap<String, Boolean>>();
 	}
 	
 	@Override
-	public void onReceive(Object arg0) throws Exception {
+	public void onReceive(Object message) throws Exception {
+		if(message instanceof Register) {
+			messageReceived((Register) message);
+		} else if(message instanceof BagCheckReport) {
+			messageReceived((BagCheckReport) message);
+		} else if(message instanceof BodyCheckReport) {
+			messageReceived((BodyCheckReport) message);
+		}
 		
+		
+	}
+	
+	
+	
+	private void messageReceived(BagCheckReport bagCheckReport) {
+		log("Received BagCheckReport message from BagCheck");
+		
+		if(!childrenAreRegistered()) {
+			//TODO
+			return;
+		}
+
+		if(!doesBaggageExist(bagCheckReport.getbaggage())) {
+			boolean allSet = bagHasArrived(bagCheckReport.getbaggage(), bagCheckReport.didPass());
+			
+			if(allSet) {
+				boolean goToJailAnswer = mapping.get(bagCheckReport.getbaggage().whoDoesThisBaggageBelongTo()).get(BAGGAGE);
+				if(goToJailAnswer) {
+					//tell person to go to jail
+				} else {
+					// exit system
+				}
+			}
+		}
+	}
+	
+	private void messageReceived(BodyCheckReport bodyCheckReport) {
+		log("Received BodyCheckReport message from BodyCheck");
+		
+		if(!childrenAreRegistered()) {
+			//TODO
+			return;
+		}
+		
+		if(!doesPassengerExist(bodyCheckReport.getPassenger())) {
+			boolean allSet = passengerHasArrived(bodyCheckReport.getPassenger());
+			
+			if(allSet) {
+				boolean goToJailAnswer = mapping.get(bodyCheckReport.getPassenger()).get(BAGGAGE);
+				if(goToJailAnswer) {
+					//tell person to go to jail
+				} else {
+					// exit system
+				}
+			}
+		}
+		
+	}
+	
+	private void messageReceived(Register register) {
+		log("Received Register message from subordinates");
+		
+		if(childrenAreRegistered()) {
+			return;
+		}
+		
+		jailActor = register.getJailActor();
+		register.getBagCheckActor(lineNumber).tell(register);
+		register.getBodyCheckActor(lineNumber).tell(register);
+	}
+	
+	
+	
+	private boolean bagHasArrived(Baggage baggage, Boolean passesSecurity) {
+		Passenger passenger = baggage.whoDoesThisBaggageBelongTo();
+		if(!mapping.containsKey(passenger)) {
+			mapping.put(passenger, new HashMap<String, Boolean>());
+			mapping.get(passenger).put(PASSENGER, null);
+			mapping.get(passenger).put(BAGGAGE, passesSecurity);
+			
+			return false; // passenger not arrived yet
+		} else {
+			mapping.get(passenger).put(BAGGAGE, passesSecurity);
+			
+			return true; // both arrived
+		}
+	}
+	
+	private boolean passengerHasArrived(Passenger passenger) {
+		if(!mapping.containsKey(passenger)) {
+			mapping.put(passenger, new HashMap<String, Boolean>());
+			mapping.get(passenger).put(PASSENGER, true);
+			mapping.get(passenger).put(BAGGAGE, null);
+			
+			return false; // baggage not arrived yet
+		} else {
+			mapping.get(passenger).put(PASSENGER, true);
+			
+			return true; // both arrived
+		}
+	}
+
+	
+	private boolean doesPassengerExist(Passenger passenger) {
+		return mapping.containsKey(passenger) && 
+				mapping.get(passenger).get(PASSENGER) != null;
+	}
+	
+	private boolean doesBaggageExist(Baggage baggage) {
+		Passenger passenger = baggage.whoDoesThisBaggageBelongTo();
+		return mapping.containsKey(passenger) &&
+				mapping.get(passenger).get(BAGGAGE) != null;
+	}
+	
+	/**
+	 * This may be worth to do before something like a Passenger is sent!
+	 */
+	private boolean childrenAreRegistered() {
+		return (jailActor != null);
+	}
+	
+	private void log(String message, String... args) {
+		String className = this.getClass().getCanonicalName();
+		System.err.printf("LOG[%s]: %s %n", className, args);
 	}
 	
 }
