7aecacaa03fffce92b58deaff0ad220c2aec7c24
alex casciani <acc1728@rit.edu>
2013-01-31 19:36:09 -0500

    Checking in message so Adam can work on Doccheck

--- Project2 SE441/src/edu/rit/se441/project2/messages/GoToLine.java
+++ Project2 SE441/src/edu/rit/se441/project2/messages/GoToLine.java
@@ -1,5 +1,16 @@
 package edu.rit.se441.project2.messages;
 
+import edu.rit.se441.project2.nonactors.Passenger;
+
 public class GoToLine {
+	private final Passenger passenger;
+	
+	public GoToLine(final Passenger passenger) {
+		this.passenger = passenger;
+	}
+	
+	public Passenger getPassenger() {
+		return passenger;
+	}
 
 }
