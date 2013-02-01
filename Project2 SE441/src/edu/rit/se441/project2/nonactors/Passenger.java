070d94b9527a29d1f6e45d6aaf4746c61da0d6ab
alex casciani <acc1728@rit.edu>
2013-01-31 20:51:42 -0500

    The Passenger should have his/her Baggage (for now its just one but this allows us to add a collection of Baggage)

--- Project2 SE441/src/edu/rit/se441/project2/nonactors/Passenger.java
+++ Project2 SE441/src/edu/rit/se441/project2/nonactors/Passenger.java
@@ -1,5 +1,28 @@
 package edu.rit.se441.project2.nonactors;
 
 public class Passenger {
+	private final Baggage baggage;
+	private final boolean passes;
+	
+	public Passenger() {
+		// This should allow many bags in the future.
+		this.baggage = new Baggage(this);
+		this.passes = false; // Sould be random
+	}
+	
+	public Passenger(final boolean passes) {
+		// This should allow many bags in the future.
+		this.baggage = new Baggage(this);
+		this.passes = passes; // Sould be random
+	}
+	
+	public Baggage getBaggage() {
+		return baggage;
+	}
+	
+	public boolean doesPassengerPass() {
+		return passes;
+	}
+	
 
 }
