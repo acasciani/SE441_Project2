0e081051542b3b1e990fdd652636a5f0545744d9
alex casciani <acc1728@rit.edu>
2013-02-01 19:35:31 -0500

    added immutable values to exit (Passenger) for when passenger exits thes ystem... should be pretty straight forward... just forget about the passenger

--- Project2 SE441/src/edu/rit/se441/project2/messages/Exit.java
+++ Project2 SE441/src/edu/rit/se441/project2/messages/Exit.java
@@ -1,5 +1,12 @@
 package edu.rit.se441.project2.messages;
 
+import edu.rit.se441.project2.nonactors.Passenger;
+
 public class Exit {
+	private final Passenger passenger;
+	
+	public Exit(final Passenger passenger) {
+		this.passenger = passenger;
+	}
 
 }
