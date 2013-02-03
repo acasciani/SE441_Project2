819dcc4e6214eeda43a0b6143a95fa7e94a09d73
ameyer8774 <ajmeyer2011@gmail.com>
2013-02-03 12:56:58 -0500

    DocumentChecker is filled out, but lacks the ability to tell if each

--- Project2 SE441/src/edu/rit/se441/project2/actors/DocumentCheckActor.java
+++ Project2 SE441/src/edu/rit/se441/project2/actors/DocumentCheckActor.java
@@ -3,18 +3,22 @@ package edu.rit.se441.project2.actors;
 import java.util.ArrayList;
 
 import edu.rit.se441.project2.messages.EndOfDay;
+import edu.rit.se441.project2.messages.GoToLine;
 import edu.rit.se441.project2.messages.Register;
 import akka.actor.ActorRef;
 import akka.actor.UntypedActor;
 
 public class DocumentCheckActor extends UntypedActor {
 	int numLines = 0;
+	int currLine = 0;
 	ArrayList<ActorRef> lineList = new ArrayList<ActorRef>();
 	
 	@Override
 	public void onReceive(Object arg0) throws Exception {
 		
-		// initialization message
+		//TODO HOW DO LINES INDIVIDUALLY REGISTER WITH DOC CHECK
+		
+		// initialization message 
 		if (arg0 instanceof Register){
 			Register reg = (Register) arg0;
 			numLines = reg.getNumberOfLines();
@@ -39,7 +43,21 @@ public class DocumentCheckActor extends UntypedActor {
 			numLines = 0;
 		}
 		
-		// TODO NEW PASSENGER MESSAGE STUFF GOES HERE
+		// receive new passenger TODO MAKE A DIFFERENT MESSAGE FOR THIS
+		if (arg0 instanceof GoToLine){
+			GoToLine msg = (GoToLine) arg0;
+			sendPassenger(msg);
+		}
+	}
+	
+	// this method is called when a new passenger must be sent to Line.
+	private void sendPassenger(GoToLine msg){
+		lineList.get(currLine).tell(msg);
+		if (currLine == (numLines - 1)){
+			currLine = 0;
+		} else {
+			currLine++;
+		}
 	}
 
 }
