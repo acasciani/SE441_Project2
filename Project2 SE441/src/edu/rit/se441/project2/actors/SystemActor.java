dff92ecb18149819777cf0ee544296f69dd9b764
alex casciani <acc1728@rit.edu>
2013-02-05 21:09:52 -0500

    adding passengers

--- Project2 SE441/src/edu/rit/se441/project2/actors/SystemActor.java
+++ Project2 SE441/src/edu/rit/se441/project2/actors/SystemActor.java
@@ -1,13 +1,51 @@
 package edu.rit.se441.project2.actors;
 
-import edu.rit.se441.project2.messages.EndOfDay;
-import edu.rit.se441.project2.messages.Initialize;
+import java.util.Stack;
+
+import scala.actors.threadpool.Arrays;
 import akka.actor.ActorRef;
 import akka.actor.UntypedActor;
+import edu.rit.se441.project2.messages.EndOfDay;
+import edu.rit.se441.project2.messages.Initialize;
+import edu.rit.se441.project2.messages.NewPassenger;
+import edu.rit.se441.project2.nonactors.Passenger;
 
 public class SystemActor extends UntypedActor {
 	ActorRef jail = null;
 	ActorRef docCheck = null;
+	
+	private void sendPassengers() throws InterruptedException {
+		String[] names = { 	"Randy", "Michael", "Tony", "Jim", "Anthony", "Andre", 
+							"Steven", "Tyson", "Boomer", "Scott", "Mick", "Nick", 
+							"Les", "Will", "Adam", "Conor", "Ian", "Alex", "John", 
+							"James", "Tom", "Vick", "Adrian", "Greg", "Cortland", 
+							"Fred", "Stevie", "Vince", "Robert", "Eli", "Peyton", 
+							"Renee", "Amy", "Jess", "Ryan", "Amber", "Sam", "Emily", 
+							"Heather", "Monica", "Suzy", "Melissa", "Jill", "Courtney",
+							"Chelsea", "Anne", "Eva", "Rebecca", "Sarah", "Laura", "Liana",
+							"Molly", "Morgan", "Alexa", "Ashley", "Briana", "Alice", 
+							"Connie", "Tessa", "Elise", "Lindsay"  };
+		
+		Stack<String> stack = new Stack<String>();
+		stack.addAll(Arrays.asList(names));
+		
+		while(!stack.isEmpty()) {
+			for(int i=0; i<4; i++) {
+				NewPassenger newPass = new NewPassenger(new Passenger(stack.pop()));
+				docCheck.tell(newPass);
+				if(stack.isEmpty()) {
+					break;
+				} else {
+					Thread.sleep(1000L);
+				}
+			}
+			
+			Thread.sleep(2000L);
+		}
+		
+	}
+	
+	
 	@Override
 	public void onReceive(Object arg0) throws Exception {
 		
