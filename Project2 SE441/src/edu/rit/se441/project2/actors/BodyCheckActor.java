9e36b386daef89e95009a3d7c555f425a0180f34
Conor Craig <cshdfree@gmail.com>
2013-02-06 01:43:07 -0500

    Added logger statement to message reception and telling.

--- Project2 SE441/src/edu/rit/se441/project2/actors/BodyCheckActor.java
+++ Project2 SE441/src/edu/rit/se441/project2/actors/BodyCheckActor.java
@@ -9,6 +9,7 @@ import edu.rit.se441.project2.messages.EndOfDay;
 import edu.rit.se441.project2.messages.GoToBodyCheck;
 import edu.rit.se441.project2.messages.Initialize;
 import edu.rit.se441.project2.messages.Register;
+import edu.rit.se441.project2.nonactors.Consts;
 import edu.rit.se441.project2.nonactors.Logger;
 import edu.rit.se441.project2.nonactors.Passenger;
 import akka.actor.ActorRef;
@@ -16,8 +17,14 @@ import akka.actor.UntypedActor;
 
 public class BodyCheckActor extends UntypedActor {
 	private static final Logger logger = new Logger(BodyCheckActor.class);
+	
+	// child of scanners is the security
+	private static final String MY_CHLDRN = Consts.NAME_ACTORS_SECURITY.value();
+	// parent is the line
+	private static final String MY_PARENT = Consts.NAME_ACTORS_LINE.value();
+	
 	private final int lineNumber;
-	private ActorRef myLine = null; //set after the init message
+	private ActorRef myLine = null; // set after the init message
 	private ActorRef mySecurity = null; // set after the init message
 	private boolean isAcceptingPassengers = true; // set and reset during the
 													// simulation
@@ -28,23 +35,24 @@ public class BodyCheckActor extends UntypedActor {
 
 	@Override
 	public void onReceive(Object arg0) throws Exception {
-		
-		
+		Consts msgReceived = Consts.DEBUG_MSG_RECEIVED;
+
 		// initialization message
 		if (arg0 instanceof Initialize) {
-			logger.debug("Received Register message from subordinate");
-			
-			if(childrenAreRegistered()) {
-				logger.error("My dependencies aren't registered yet!");
-				//TODO what should we do here?
-				//returning is fine
+			logger.debug(msgReceived, Consts.NAME_MESSAGES_INIT, MY_CHLDRN);
+
+			if (childrenAreRegistered()) {
+				logger.error(Consts.DEBUG_MSG_CHLD_ALR_REG, MY_CHLDRN);
+				// TODO what should we do here?
+				// returning is fine
 				return;
 			}
 
 			Initialize init = (Initialize) arg0;
 			mySecurity = init.getSecurityActor(this.lineNumber);
 			myLine = init.getLineActor(lineNumber);
-			
+
+			logger.debug(Consts.DEBUG_MSG_REG_MY_CHILD, MY_PARENT);
 			myLine.tell(new Register(1));
 		}
 
@@ -52,49 +60,66 @@ public class BodyCheckActor extends UntypedActor {
 		 * Message From Line
 		 */
 		if (arg0 instanceof GoToBodyCheck) {
-			logger.debug("Received GoToBodyCheck message from Line");
-			this.isAcceptingPassengers = false;
+			if (!childrenAreRegistered()) {
+				logger.error(Consts.ERROR_MSG_CHLD_NOT_REG, MY_CHLDRN);				
+				// TODO what should we do here?
+				// returning is fine
+				return;
+			}
 			
-			//critical problem area below
+			logger.debug(msgReceived, Consts.NAME_MESSAGES_GO_TO_BODY_CHECK,
+					MY_CHLDRN);
+			
+			this.isAcceptingPassengers = false;
+
+			// critical problem area below
 			GoToBodyCheck GoToBodyCheck = (GoToBodyCheck) arg0;
 			performBodyCheck(GoToBodyCheck.getPassenger());
-			//end critical problem area
+			// end critical problem area
 		}
 
 		/*
 		 * Message From Line
 		 */
 		if (arg0 instanceof CanISendYouAPassenger) {
-			logger.debug("Received CanISendYouAPassenger message from Line");
+			logger.debug(msgReceived,
+					Consts.NAME_MESSAGES_CAN_I_SEND_YOU_A_PASSENG, MY_CHLDRN);
 			if (this.isAcceptingPassengers) {
 				CanISendYouAPassenger cISYAP = (CanISendYouAPassenger) arg0;
 				ActorRef myLine = cISYAP.getLineActor();
+				
+				logger.debug(Consts.NAME_MESSAGES_BODY_CHECK_REQUESTS_NEXT, MY_PARENT);
 				myLine.tell(new BodyCheckRequestsNext());
 			} else {
 				// swallow the message
 			}
 		}
 
-		
 		if (arg0 instanceof EndOfDay) {
-			logger.debug("Received EndOfDay message from Line");
-			//TODO what to do here?
+			logger.debug(msgReceived, Consts.NAME_MESSAGES_END_OF_DAY,
+					MY_CHLDRN);
+			// TODO what to do here?
 			shutDown();
-			
+
 		}
 	}
 
-
 	private void shutDown() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	//Function takes a passenger and messages Security whether it passes or fails
-	//BodyCheckActor is accepting passengers after this message
+	// Function takes a passenger and messages Security whether it passes or
+	// fails
+	// BodyCheckActor is accepting passengers after this message
 	private void performBodyCheck(Passenger p) {
 		double n = Math.random();
 		BodyCheckReport myBodyReport;
+		
+		Consts bodyChkRptLbl = Consts.NAME_MESSAGES_BODY_CHECK_REPORT;
+		Consts securityLbl = Consts.NAME_ACTORS_SECURITY;
+		Consts bodyChkLbl = Consts.NAME_ACTORS_BODY_CHECK;
+		
 		if (n > .5) {
 			// Passes scan
 			myBodyReport = new BodyCheckReport(p, true);
@@ -102,11 +127,13 @@ public class BodyCheckActor extends UntypedActor {
 			// Fails scan
 			myBodyReport = new BodyCheckReport(p, false);
 		}
+		
+		logger.debug(Consts.DEBUG_MSG_SEND_OBJ_TO_IN_MESS, bodyChkRptLbl, securityLbl, bodyChkLbl);
 		this.mySecurity.tell(myBodyReport);
 		this.isAcceptingPassengers = true;
 
 	}
-	
+
 	/**
 	 * This may be worth to do before something like a Passenger is sent!
 	 */
--- Project2 SE441/src/edu/rit/se441/project2/actors/DocumentCheckActor.java
+++ Project2 SE441/src/edu/rit/se441/project2/actors/DocumentCheckActor.java
@@ -7,107 +7,123 @@ import edu.rit.se441.project2.messages.GoToLine;
 import edu.rit.se441.project2.messages.Initialize;
 import edu.rit.se441.project2.messages.NewPassenger;
 import edu.rit.se441.project2.messages.Register;
+import edu.rit.se441.project2.nonactors.Consts;
+import edu.rit.se441.project2.nonactors.Logger;
 import edu.rit.se441.project2.nonactors.Passenger;
 import akka.actor.ActorRef;
 import akka.actor.UntypedActor;
 
 public class DocumentCheckActor extends UntypedActor {
+	private static final Logger logger = new Logger(LineActor.class);
+	private static final String MY_CHLDRN = Consts.NAME_ACTORS_LINE.value();
+	private static final String MY_PARENT = Consts.NAME_ACTORS_SYSTEM.value();
 	int numLines = 0;
 	int currLine = 0;
 	ArrayList<ActorRef> lineList = new ArrayList<ActorRef>();
 	boolean[] lineStatus;
 	boolean linesReady = false;
 	ActorRef system;
-	
+
 	@Override
 	public void onReceive(Object arg0) throws Exception {
-		
-		// initialization message 
-		if (arg0 instanceof Initialize){
+		Consts msgReceived = Consts.DEBUG_MSG_RECEIVED;
+
+		// initialization message
+		if (arg0 instanceof Initialize) {
+			logger.debug(msgReceived, Consts.NAME_MESSAGES_INIT, MY_CHLDRN);
+
 			Initialize init = (Initialize) arg0;
 			numLines = init.getNumberOfLines();
-			
+
 			// create a list of the lines
-			for (int x = 0; x < numLines; x++){
+			for (int x = 0; x < numLines; x++) {
 				lineList.add(init.getLineActor(x));
 			}
-			
+
 			// create a status list for the lines
 			lineStatus = new boolean[numLines];
-			
+
 			// remember the system actor reference
 			system = init.getSystemActor();
 		}
-		
+
 		// line register message
-		if (arg0 instanceof Register){
+		if (arg0 instanceof Register) {
+			//logger.debug(msgReceived, Consts.NAME_MESSAGES_REGISTER, MY_CHLDRN);
+			//the message must be done in the register() method
 			Register reg = (Register) arg0;
 			int sender = reg.getSender();
 			lineStatus[sender] = true;
-			
+
 			// are all lines ready?
-			for (int x = 0; x < numLines; x++){
-				if (!lineStatus[x]){
+			for (int x = 0; x < numLines; x++) {
+				if (!lineStatus[x]) {
 					break;
 				}
-				
+
 				// if so, flip the switch and register with systemActor
 				linesReady = true;
 				register();
 			}
 		}
-		
+
 		// end of day message
-		if (arg0 instanceof EndOfDay){
-			
-			//pass the message on to the Lines
-			for (int x = 0; x < numLines; x++){
+		if (arg0 instanceof EndOfDay) {
+			logger.debug(msgReceived, Consts.NAME_MESSAGES_END_OF_DAY,
+					MY_CHLDRN);
+
+			// pass the message on to the Lines
+			for (int x = 0; x < numLines; x++) {
 				lineList.get(x).tell(arg0);
 			}
-			
-			//erase the lineList and reset numLines to zero.
+
+			// erase the lineList and reset numLines to zero.
 			lineList = null;
 			numLines = 0;
 		}
-		
+
 		// receive new passenger
-		if (arg0 instanceof NewPassenger){
+		if (arg0 instanceof NewPassenger) {
+			// see sendPassenger message
 			NewPassenger msg = (NewPassenger) arg0;
 			Passenger pass = msg.getPassenger();
 			sendPassenger(pass);
 		}
 	}
-	
+
 	// this method is called when a new passenger must be sent to Line.
-	private void sendPassenger(Passenger pass){
+	private void sendPassenger(Passenger pass) {
 		boolean atLeastOneLineOpen = false;
-		
+
 		// make sure at the lines are functioning
-		for (int x = 0; x < numLines; x++){
-			if (lineStatus[currLine]){
+		for (int x = 0; x < numLines; x++) {
+			if (lineStatus[currLine]) {
 				atLeastOneLineOpen = true;
 			}
 		}
-		
+
 		// if not, the passenger fails to send. tell the console.
-		if (!atLeastOneLineOpen){
-			System.err.println("FAILED TO SEND A PASSENGER TO LINE, AS NO LINES ARE CURRENTLY OPEN!");
+		if (!atLeastOneLineOpen) {
+			System.err
+					.println("FAILED TO SEND A PASSENGER TO LINE, AS NO LINES ARE CURRENTLY OPEN!");
 			return;
 		}
-		
+
 		// send the passenger
 		GoToLine msg = new GoToLine(pass);
+		logger.debug(Consts.DEBUG_MSG_ADD_PASS_TO_QUEUE, MY_CHLDRN);
 		lineList.get(currLine).tell(msg);
-		
+
 		// iterate to the next line in the group
-		if (currLine == (numLines - 1)){
+		if (currLine == (numLines - 1)) {
 			currLine = 0;
 		} else {
 			currLine++;
 		}
 	}
 
-	private void register(){
+	private void register() {
+		logger.debug(Consts.NAME_MESSAGES_REGISTER, MY_PARENT);
 		system.tell(new Register(0));
 	}
 }
