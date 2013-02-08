8f1b34e20d2e4e9146052b4aeffcb4271a91a996
Conor Craig <cshdfree@gmail.com>
2013-02-07 21:00:31 -0500

    Added sendFewPassengers() to SystemActor.

--- Project2 SE441/src/edu/rit/se441/project2/actors/SystemActor.java
+++ Project2 SE441/src/edu/rit/se441/project2/actors/SystemActor.java
@@ -19,25 +19,24 @@ import edu.rit.se441.project2.nonactors.Logger;
 import edu.rit.se441.project2.nonactors.Passenger;
 
 public class SystemActor extends UntypedActor {
-	private static final Logger logger = new Logger(SecurityActor.class);
+	private static final Logger logger = new Logger(SystemActor.class);
 	ActorRef jail = null;
 	ActorRef docCheck = null;
+	String[] names = { "Randy", "Michael", "Tony", "Jim", "Anthony", "Andre",
+			"Steven", "Tyson", "Boomer", "Scott", "Mick", "Nick", "Les",
+			"Will", "Adam", "Conor", "Ian", "Alex", "John", "James", "Tom",
+			"Vick", "Adrian", "Greg", "Cortland", "Fred", "Stevie", "Vince",
+			"Robert", "Eli", "Peyton", "Renee", "Amy", "Jess", "Ryan", "Amber",
+			"Sam", "Emily", "Heather", "Monica", "Suzy", "Melissa", "Jill",
+			"Courtney", "Chelsea", "Anne", "Eva", "Rebecca", "Sarah", "Laura",
+			"Liana", "Molly", "Morgan", "Alexa", "Ashley", "Briana", "Alice",
+			"Connie", "Tessa", "Elise", "Lindsay" };
 
 	/**
 	 * This method creates and sends four passengers to Document Checking at two
 	 * second intervals until there are no passenger names left to go through.
 	 */
 	private void sendPassengers() throws InterruptedException {
-		String[] names = { "Randy", "Michael", "Tony", "Jim", "Anthony",
-				"Andre", "Steven", "Tyson", "Boomer", "Scott", "Mick", "Nick",
-				"Les", "Will", "Adam", "Conor", "Ian", "Alex", "John", "James",
-				"Tom", "Vick", "Adrian", "Greg", "Cortland", "Fred", "Stevie",
-				"Vince", "Robert", "Eli", "Peyton", "Renee", "Amy", "Jess",
-				"Ryan", "Amber", "Sam", "Emily", "Heather", "Monica", "Suzy",
-				"Melissa", "Jill", "Courtney", "Chelsea", "Anne", "Eva",
-				"Rebecca", "Sarah", "Laura", "Liana", "Molly", "Morgan",
-				"Alexa", "Ashley", "Briana", "Alice", "Connie", "Tessa",
-				"Elise", "Lindsay" };
 
 		Stack<String> stack = new Stack<String>();
 		stack.addAll(Arrays.asList(names));
@@ -53,10 +52,10 @@ public class SystemActor extends UntypedActor {
 				times++;
 				NewPassenger newPass = new NewPassenger(new Passenger(
 						stack.pop()));
-				
+
 				logger.debug("System sends a NewPassenger message.");
 				docCheck.tell(newPass);
-				
+
 				if (stack.isEmpty()) {
 					break;
 				} else {
@@ -78,45 +77,61 @@ public class SystemActor extends UntypedActor {
 
 		// Initialization message
 		if (arg0 instanceof Initialize) {
-			
+
 			logger.debug("System has received an Initialize message.");
 			Initialize init = (Initialize) arg0;
-			
+
 			// Extract the actor references
 			jail = init.getJailActor();
 			docCheck = init.getDocumentCheckActor();
 
 			// Begin the registration process for jail and docCheck
 			logger.debug("System has sent an initialize method to jail and docCheck.");
-			
+
 			jail.tell(init);
 			docCheck.tell(init);
 		}
 
 		// End of day message
 		else if (arg0 instanceof EndOfDay) {
-			
+
 			logger.debug("System has received an EndOfDay message.");
-			
+
 			// pass the message on to docCheck
 			logger.debug("System has sent an EndOfDay message to docCheck.");
-			
+
 			docCheck.tell(arg0);
 		}
-		
+
 		// Register message
 		else if (arg0 instanceof Register) {
-		
+
 			logger.debug("System has received a Register message.");
-			
+
 			// pass the message on to docCheck
-			sendPassengers();
+			// sendPassengers();
+			sendFewPassengers();
 		}
+	}
+
+	private void sendFewPassengers() {
+		int times = 0;
+		for (;;) {
+			if (times == 2) {
+				break;
+			}
+			NewPassenger newPass = new NewPassenger(new Passenger(names[times]));
+			logger.debug("System sends a NewPassenger message.");
+			docCheck.tell(newPass);
+			times++;
+		}
+		logger.debug("System sends an EndOfDay message.");
+		docCheck.tell(new EndOfDay());
 	}
 
 	/*
 	 * Function returns the class's equivalent CONST from constants.java
-	 * */
+	 */
 	public String toString() {
 		return Consts.NAME_ACTORS_SYSTEM.value();
 	}
