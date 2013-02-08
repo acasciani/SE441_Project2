5fa6d92f722000c93cc2e435bc52165bc57bd485
ameyer8774 <ajmeyer2011@gmail.com>
2013-02-07 21:03:49 -0500

    Jail now has logging capabilities

--- Project2 SE441/src/edu/rit/se441/project2/actors/JailActor.java
+++ Project2 SE441/src/edu/rit/se441/project2/actors/JailActor.java
@@ -6,6 +6,7 @@ import edu.rit.se441.project2.messages.EndOfDay;
 import edu.rit.se441.project2.messages.GoToJail;
 import edu.rit.se441.project2.messages.Initialize;
 import edu.rit.se441.project2.nonactors.Consts;
+import edu.rit.se441.project2.nonactors.Logger;
 import edu.rit.se441.project2.nonactors.Passenger;
 import akka.actor.ActorRef;
 import akka.actor.UntypedActor;
@@ -14,6 +15,8 @@ public class JailActor extends UntypedActor {
 	int numLines;
 	ArrayList<ActorRef> securityList = null;
 	ArrayList<Passenger> prisonerList = null;
+	private static final Logger logger = new Logger(JailActor.class);
+
 
 	/*
 	 * Function processes incoming message types in form of an Object class.
@@ -24,7 +27,11 @@ public class JailActor extends UntypedActor {
 
 		// initialization message
 		if (arg0 instanceof Initialize) {
+			
+			logger.debug("Jail received an Initialize message.");
+			
 			Initialize init = (Initialize) arg0;
+			
 			numLines = init.getNumberOfLines();
 			securityList = new ArrayList<ActorRef>();
 			prisonerList = new ArrayList<Passenger>();
@@ -32,12 +39,17 @@ public class JailActor extends UntypedActor {
 			// pass message to security and remember the security actors
 			for (int x = 0; x < numLines; x++) {
 				securityList.add(x, init.getSecurityActor(x));
+				
+				logger.debug("Jail sent an Initialize message to Security " + x + ".");
 				init.getSecurityActor(x).tell(init);
 			}
 		}
 
 		// new prisoner message
 		if (arg0 instanceof GoToJail) {
+			
+			logger.debug("Jail received a GoToJail message.");
+			
 			GoToJail msg = (GoToJail) arg0;
 
 			// extract the passenger and insert them into the list of prisoners
@@ -48,6 +60,8 @@ public class JailActor extends UntypedActor {
 		// end of day message
 		if (arg0 instanceof EndOfDay) {
 
+			logger.debug("Jail received an EndOfDay message.");
+			
 			// empty the prisonerList and the securityList
 			securityList = null;
 			prisonerList = null;
